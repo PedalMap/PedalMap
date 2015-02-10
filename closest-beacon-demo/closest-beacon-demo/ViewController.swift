25f461eb16333e1f1c7830400bc8b2c1045dce1c
Zachary Browne <browne.zachary@gmail.com>
2015-02-09 22:55:15 -0800

    First commit for new beacon project demo

---
+++ closest-beacon-demo/closest-beacon-demo/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  closest-beacon-demo
+//
+//  Created by Zachary Browne on 2/7/15.
+//  Copyright (c) 2015 Zachary Browne. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    let region = CLBeaconRegion(proximityUUID:
+        NSUUID(UUIDString: "E2C56-DFFB-48D2-B060-D0F5A71096E0"), identifier: "AprilBrothers")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // TODO: Add case for when user denies authorization
+        locationManager.delegate = self;
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse) {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        locationManager.startRangingBeaconsInRegion(region)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
+        println(beacons)
+    }
+}
+
