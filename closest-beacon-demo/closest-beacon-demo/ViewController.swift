e40f44dfa07beb59dbefa03127536a3825690fcb
Zachary Browne <browne.zachary@gmail.com>
2015-03-15 16:18:12 -0700

    Added different colors for different minor values, filtered out unknown beacons from beacon array, and set background color to colors associated beacon minor values depending on nearest beacon location

--- closest-beacon-demo/closest-beacon-demo/ViewController.swift
+++ closest-beacon-demo/closest-beacon-demo/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     let locationManager = CLLocationManager()
     let region = CLBeaconRegion(proximityUUID:
         NSUUID(UUIDString: "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0"), identifier: "AprilBrothers")
+    let colors = [
+        0: UIColor(red: 142/255, green: 212/255, blue: 220/255, alpha: 1),
+        1: UIColor(red: 162/255, green: 213/255, blue: 181/255, alpha: 1)
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +36,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
-        println(beacons)
+        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
+        // NOTE: This assumes that closest beacon is first in the array. Not fit for production
+        if (knownBeacons.count > 0) {
+            let closestBeacon = knownBeacons[0] as CLBeacon
+            self.view.backgroundColor = self.colors[closestBeacon.minor.integerValue]
+            println(beacons)
+        }
     }
 }
 
